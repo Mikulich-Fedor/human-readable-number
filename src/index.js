5e6e2b296ee1eb554322de73d61a217c16dd5ea1
fiodar miculich <feotheodor@gmail.com>
2020-10-17 17:31:14 +0300

    init: task number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
+    let numberOneArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let numberDecArray = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    let ten = number % 100;
+    let result = " ";
+    // if we have hundred
+    if(Math.trunc(number / 100)){
+        result = numberOneArray[Math.trunc(number / 100)] + " hundred " ;
+    };
+    // if we dont have some ten
+    if(result !== "" && ten === 0){
+        return result.trim();
+    }
+    // if we heve ten 
+    if(ten < 20){
+        result += numberOneArray[ten];
+    }else{
+        result += numberDecArray[Math.trunc(ten / 10) - 1] + " " + ((ten%10) ? numberOneArray[ten % 10] : "" ) 
+    }
+    return result.trim();
+
 }
